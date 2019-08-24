49c981da5f5d42c46e6f02b8899ab92f19f7dc78
medyasun <msungur@gmail.com>
2019-08-24 21:20:04 +0300

    aram duzeltmeleri ve ufak duzeltmeler

--- test.js
+++ test.js
@@ -49,10 +49,10 @@ app.use('/sonuclar', (req, res) => {
     var arama = req.body.kelime.toLocaleLowerCase('tr-TR');
     var aramaSplit=[];
     aramaSplit=arama.split(' ');
-    aramaLength=aramaSplit.length;
+    var aramaLength=aramaSplit.length;
     var urlarama = urlify(arama);
     const url = "https://www.migros.com.tr/arama?q="+urlarama;
-    const url2 ="https://www.carrefoursa.com/tr/search?q="+urlarama+"%3Arelevance%3AinStockFlag%3Atrue&show=All";
+    const url2 ="https://www.carrefoursa.com/tr/search/?q="+urlarama+"Arelevance%3AinStockFlag%3Atrue&text="+urlarama+"#";
     const url3 ="https://www.a101.com.tr/list/?search_text="+urlarama+"#/";
     const url4 ="https://www.bim.com.tr/Categories/100/aktuel-urunler.aspx";
     const url5 = "https://www.happycenter.com.tr/Product/Search/?ara="+urlarama;
@@ -74,18 +74,24 @@ app.use('/sonuclar', (req, res) => {
     let fiyat,fiyat2,fiyat3,fiyat4,fiyat5;
     let search,search2,search3,search4,search5;
     //#endregion
+    console.log("tanımlar bitti");
     //---------------------------------------------------------------------------------------------------------------
-    //#region migros
+    //#region migros 1
     const urlElems = $(
       "#page-area > div > div > div.col-md-10.category-right > div.sub-category-product-list > div"
     );
+    var urlEL=urlElems.length;
+    var aramaBirlestir;
+    var j;
+    var i;
+    for (i=1, j=0; i<=urlEL && j<aramaLength; j++, i=(j==aramaLength)?i+1:i,j=(j==aramaLength)?j=0:j) {
 
-    for (let i = 1; i <= urlElems.length; i++) {
       search = $(
         "#page-area > div > div > div.col-md-10.category-right > div.sub-category-product-list > div.list.p" +
         [i] +
         " > div > form > div.center.product-card-center > h5 > a"
-      ).text();
+      ).text()
+      .toLocaleLowerCase('tr-TR');
 
       fiyat = $(
 
@@ -103,40 +109,46 @@ app.use('/sonuclar', (req, res) => {
 
       jsonSonuclar = {
         ID  : 1,
+        logo:"https://sanalmarket-com-web-assets.global.ssl.fastly.net/images/m.df813fa8.png",
         site: "migros",
         name: search,
         Price: parseFloat(fiyat.replace(",", "."), 10)
       };
 
-      var aramaBirlestir;
-      for (let i = 0; i < aramaLength; i++) {
-        if(i==0){ aramaBirlestir=jsonSonuclar.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])+"&"}
-        else if(aramaLength-1==i){  aramaBirlestir+=jsonSonuclar.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])}
-        else { aramaBirlestir+=jsonSonuclar.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])+"&"}
-      }
+
+        if(j==0){ aramaBirlestir=jsonSonuclar.name.includes(aramaSplit[j])+"&"}
+        else if(aramaLength-1==j){  aramaBirlestir+=jsonSonuclar.name.includes(aramaSplit[j])}
+        else { aramaBirlestir+=jsonSonuclar.name.includes(aramaSplit[j])+"&"}
+      
       if (
         !aramaBirlestir.includes('false')
         ) sonuclar.push(jsonSonuclar);
     
     }
     //#endregion migros
+    console.log("migros bitti");
     //---------------------------------------------------------------------------------------------------------------
-    //#region carefour
+    //#region carefour 2
   const urlElems2 = $2(
     "body > main > div:nth-child(8) > div:nth-child(2) > div.col-xs-12.col-sm-12.col-md-9.col-lg-10 > div.pl-grid-cont.row > ul>li > div"
   );
-  for (let i = 1; i <= urlElems2.length; i++) {
+var urlE2L=urlElems2.length;
+var aramaBirlestir2;
+var j2;
+var i2;
+for (i2=1, j2=0; i2<=urlE2L && j2<aramaLength; j2++, i2=(j2==aramaLength)?i2+1:i2,j2=(j2==aramaLength)?j2=0:j2) {
     search2 = $2(
       "body > main > div:nth-child(8) > div:nth-child(2) > div.col-xs-12.col-sm-12.col-md-9.col-lg-10 > div.pl-grid-cont.row > ul > li:nth-child(" +
-        [i] +
+        [i2] +
         ") > div > div > div > div.product_click > a > span.item-name"
     )
       .text()
-      .trim();
+      .trim()
+      .toLocaleLowerCase('tr-TR');
 
     fiyat2 = $2(
       "body > main > div:nth-child(8) > div:nth-child(2) > div.col-xs-12.col-sm-12.col-md-9.col-lg-10 > div.pl-grid-cont.row > ul > li:nth-child(" +
-        [i] +
+        [i2] +
         ") > div > div > div > div.product_click > a > span.dis-price-cont.clearfix > span > span.item-price"
     )
       .text()
@@ -149,36 +161,42 @@ app.use('/sonuclar', (req, res) => {
 
     jsonSonuclar2 = {
       ID  : 2,
+      logo:"https://reimg-carrefour.mncdn.com/staticimage/favicon.ico",
       site: "carefour",
       name: search2,
       Price: parseFloat(fiyat2.replace(",", "."), 10)
     };
 
-    var aramaBirlestir2;
-    for (let i = 0; i < aramaLength; i++) {
-      if(i==0){ aramaBirlestir2=jsonSonuclar2.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])+"&"}
-      else if(aramaLength-1==i){  aramaBirlestir2+=jsonSonuclar2.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])}
-      else { aramaBirlestir2+=jsonSonuclar2.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])+"&"}
-    }
+      if(j2==0){ aramaBirlestir2=jsonSonuclar2.name.includes(aramaSplit[j2])+"&"}
+      else if(aramaLength-1==j2){  aramaBirlestir2+=jsonSonuclar2.name.includes(aramaSplit[j2])}
+      else { aramaBirlestir2+=jsonSonuclar2.name.includes(aramaSplit[j2])+"&"}
+
     if (
       !aramaBirlestir2.includes('false')
       ) sonuclar.push(jsonSonuclar2);
   }
   //#endregion
+  console.log("carrefour bitti");
     //---------------------------------------------------------------------------------------------------------------
-    //#region A101
+    //#region A101 3
 
 const urlElems3 = $3(
   "body > div.js-main-wrapper > div.page-list.js-container > div:nth-child(3) > div.content > div > div.col-md-10.col-sm-9 > div> div > div >div"
 );
-for (let i = 1; i <= urlElems3.length; i++) {
+var urlE3L=urlElems3.length;
+var aramaBirlestir3;
+var j;
+var i;
+for (i=1, j=0; i<=urlE3L && j<aramaLength; j++, i=(j==aramaLength)?i+1:i,j=(j==aramaLength)?j=0:j) {
+
   search3 = $3(
     "body > div.js-main-wrapper > div.page-list.js-container > div:nth-child(3) > div.content > div > div.col-md-10.col-sm-9 > div > div > div > div:nth-child(" +
     [i] +
     ") > div > a > div > h3"
   )
     .text()
-    .trim();
+    .trim()
+    .toLocaleLowerCase('tr-TR');
 
   fiyat3 = $3(
     "body > div.js-main-wrapper > div.page-list.js-container > div:nth-child(3) > div.content > div > div.col-md-10.col-sm-9 > div > div > div > div:nth-child(" +
@@ -195,6 +213,7 @@ for (let i = 1; i <= urlElems3.length; i++) {
 
   jsonSonuclar3 = {
     ID  : 3,
+    logo:"https://ayb.akinoncdn.com/static_omnishop/ayb288/assets/img/favicon.ico",
     site: "A101",
     name: search3,
     Price: parseFloat(fiyat3.replace(",", "."), 10)
@@ -202,25 +221,28 @@ for (let i = 1; i <= urlElems3.length; i++) {
 
   if (isNaN(jsonSonuclar3.Price)) jsonSonuclar3.Price = 999 ;
 
-  var aramaBirlestir3;
-  for (let i = 0; i < aramaLength; i++) {
-    if(i==0){ aramaBirlestir3=jsonSonuclar3.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])+"&"}
-    else if(aramaLength-1==i){  aramaBirlestir3+=jsonSonuclar3.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])}
-    else { aramaBirlestir3+=jsonSonuclar3.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])+"&"}
-  }
+
+    if(j==0){ aramaBirlestir3=jsonSonuclar3.name.includes(aramaSplit[j])+"&"}
+    else if(aramaLength-1==j){  aramaBirlestir3+=jsonSonuclar3.name.includes(aramaSplit[j])}
+    else { aramaBirlestir3+=jsonSonuclar3.name.includes(aramaSplit[j])+"&"}
+  
   if (
     !aramaBirlestir3.includes('false')
     ) sonuclar.push(jsonSonuclar3);
 
 }
 //#endregion
+console.log("a101 bitti");
     //---------------------------------------------------------------------------------------------------------------
     //#region happy center 5
 const urlElems5 = $5(
   "#product-grid-list > div.panel-body > div> div"
 );
-
-for (let i = 1; i <= urlElems5.length; i++) {
+var urlE5L=urlElems5.length;
+var aramaBirlestir5;
+var j;
+var i;
+for (i=1, j=0; i<=urlE5L && j<aramaLength; j++, i=(j==aramaLength)?i+1:i,j=(j==aramaLength)?j=0:j) {
   search5 = $5(
     "#product-grid-list > div.panel-body > div > div:nth-child(" +
     [i] +
@@ -228,7 +250,8 @@ for (let i = 1; i <= urlElems5.length; i++) {
   ) 
   .text()
   .trim()
-  .replace("\n", "");
+  .replace("\n", "")
+  .toLocaleLowerCase('tr-TR');
 
   fiyat5 = $5(
     "#product-grid-list > div.panel-body > div > div:nth-child(" +
@@ -245,17 +268,17 @@ for (let i = 1; i <= urlElems5.length; i++) {
 
   jsonSonuclar5 = {
     ID  : 5,
+    logo:"https://static.happycenter.com.tr/Uploads/favicon.PNG",
     site: "happycenter",
     name: search5,
     Price: parseFloat(fiyat5.replace(",", "."), 10)
   };
 
-  var aramaBirlestir5;
-  for (let i = 0; i < aramaLength; i++) {
-    if(i==0){ aramaBirlestir5=jsonSonuclar5.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])+"&"}
-    else if(aramaLength-1==i){  aramaBirlestir5+=jsonSonuclar5.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])}
-    else { aramaBirlestir5+=jsonSonuclar5.name.toLocaleLowerCase('tr-TR').includes(aramaSplit[i])+"&"}
-  }
+  
+    if(j==0){ aramaBirlestir5=jsonSonuclar5.name.includes(aramaSplit[j])+"&"}
+    else if(aramaLength-1==j){  aramaBirlestir5+=jsonSonuclar5.name.includes(aramaSplit[j])}
+    else { aramaBirlestir5+=jsonSonuclar5.name.includes(aramaSplit[j])+"&"}
+  
   if (
     !aramaBirlestir5.includes('false')&& typeof jsonSonuclar5!="undefined"
     ) {sonuclar.push(jsonSonuclar5)
@@ -264,20 +287,26 @@ for (let i = 1; i <= urlElems5.length; i++) {
     
 }
 //#endregion
+console.log("happy center bitti");
     //---------------------------------------------------------------------------------------------------------------
-    //#region bim aktuel
+    //#region bim aktuel 4
 
  const urlElems4 = $4(
   "#form1 > div:nth-child(11) > div > div.container.content.white.no-pb > div>div"
 );
-for (let i = 1; i <= urlElems4.length; i++) {
+var urlE4L=urlElems4.length;
+var aramaBirlestir4;
+var j;
+var i;
+for (i=1, j=0; i<=urlE4L && j<aramaLength; j++, i=(j==aramaLength)?i+1:i,j=(j==aramaLength)?j=0:j) {
   search4 = $4(
     "#form1 > div:nth-child(11) > div > div.container.content.white.no-pb > div > div:nth-child(" +
     [i] +
     ") > h4 > a"
   )
     .text()
-    .trim();
+    .trim()
+    .toLocaleLowerCase('tr-TR');
 
   fiyat4 = $4(
     "#form1 > div:nth-child(11) > div > div.container.content.white.no-pb > div > div:nth-child(" +
@@ -294,6 +323,7 @@ for (let i = 1; i <= urlElems4.length; i++) {
 
   jsonSonuclar4 = {
     ID  : 4,
+    logo:"https://www.bim.com.tr/Uploads/logo.ico",
     site: "BİM Aktuel",
     name: search4,
     Price: parseFloat(fiyat4.replace(",", "."), 10)
@@ -301,32 +331,34 @@ for (let i = 1; i <= urlElems4.length; i++) {
 
   if (isNaN(jsonSonuclar4.Price)) jsonSonuclar4.Price = 999 ;
 
-  var aramaBirlestir4;
-  for (let i = 0; i < aramaLength; i++) {
-    if(i==0){ aramaBirlestir4=jsonSonuclar4.name.toLowerCase().includes(aramaSplit[i])+"&"}
-    else if(aramaLength-1==i){  aramaBirlestir4+=jsonSonuclar4.name.toLowerCase().includes(aramaSplit[i])}
-    else { aramaBirlestir4+=jsonSonuclar4.name.toLowerCase().includes(aramaSplit[i])+"&"}
-  }
+ 
+    if(j==0){ aramaBirlestir4=jsonSonuclar4.name.includes(aramaSplit[j])+"&"}
+    else if(aramaLength-1==j){  aramaBirlestir4+=jsonSonuclar4.name.includes(aramaSplit[j])}
+    else { aramaBirlestir4+=jsonSonuclar4.name.includes(aramaSplit[j])+"&"}
+  
   if (
     !aramaBirlestir4.includes('false')
     ) sonuclar.push(jsonSonuclar4);
 
 }
 //#endregion
+console.log("bim bitti");
     //---------------------------------------------------------------------------------------------------------------
 sonuclar.sort(function(a, b) {
   return a.Price - b.Price;
 });
 
-for (let i = 1; i <= 5; i++) {      // ---------- eger sistemi yorarsa bu alani site adedi ile degistir
+var k;
+for (k = 1; k <= 5; k++) {      // ---------- eger sistemi yorarsa bu alani site adedi ile degistir
  
-  sonuclartmp=sonuclar.filter(function(sonuc) {return sonuc.ID == i;});
+  sonuclartmp=sonuclar.filter(function(sonuc) {return sonuc.ID == k;});
   enucuzSonuclar.push(sonuclartmp[0]);
   sonuclartest.push(sonuclartmp) ;
 }
 
 enucuzSonuclar=enucuzSonuclar.filter(function(sonuc) {return sonuc!=null;});
 
+console.log("enucuz sonuclar bitti");
 //res.sendFile(path.join(__dirname, "index.html"));
     var data = fs.readFileSync('index.html').toString();
     data = data.replace("---datagonder---",JSON.stringify(enucuzSonuclar) );
